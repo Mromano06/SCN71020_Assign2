64f3faafdb523ad872f9a03ea3f1404d38c4fe92
Mromano06 <matbat2205@gmail.com>
2024-10-13 10:18:51 -0400

    Added test project

---
+++ RecUnitTest/RecUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RecUnitTest
+{
+	TEST_CLASS(RecUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
